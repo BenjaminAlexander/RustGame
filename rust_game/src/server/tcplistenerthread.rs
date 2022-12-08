b9170639abbf934f1cb350bd6732c4d233920391
BenjaminAlexander <benalexander71792@gmail.com>
2022-12-07 19:21:31 -0800

    Removing unwrap

--- rust_game/src/server/tcplistenerthread.rs
+++ rust_game/src/server/tcplistenerthread.rs
@@ -24,14 +24,30 @@ impl<Game: GameTrait> ChannelThread<()> for TcpListenerThread<Game> {
     fn run(mut self, receiver: Receiver<Self>) {
         let socket_addr_v4:SocketAddrV4 = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), Game::TCP_PORT);
         let socket_addr:SocketAddr = SocketAddr::from(socket_addr_v4);
-        let listener:TcpListener = TcpListener::bind(socket_addr).unwrap();
+
+
+        let listener = match TcpListener::bind(socket_addr) {
+            Ok(tcp_listener) => tcp_listener,
+            Err(error) => {
+                error!("{:?}", error);
+                return;
+            }
+        };
 
         // accept connections and process them serially
         for result in listener.incoming() {
             match result {
                 Ok(tcp_stream) => {
-                    info!("New TCP connection from {:?}", tcp_stream.peer_addr().unwrap().ip().to_string());
-                    //core.addTcpStream(tcpStream);
+
+                    match tcp_stream.peer_addr() {
+                        Ok(socket_addr) => {
+                            info!("New TCP connection from {:?}", socket_addr.ip().to_string());
+                        }
+                        Err(error) => {
+                            error!("Unable to get tcp stream peer address");
+                            error!("{:?}", error);
+                        }
+                    }
 
                     //TODO: this doesn't really do anything, should probably check if listening should stop
                     receiver.try_iter(&mut self);
