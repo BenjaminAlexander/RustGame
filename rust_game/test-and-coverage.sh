33a6ede40a26bc7d118dcbec2086534751a742a2
Ben Alexander <benalexander71792@gmail.com>
2023-12-30 20:12:21 -0800

    fixing some stuff

--- rust_game/test-and-coverage.sh
+++ rust_game/test-and-coverage.sh
@@ -7,4 +7,4 @@ export LLVM_PROFILE_FILE="$PWD/target/coverage/cargo-test-%p-%m.profraw"
 cargo clean
 cargo test
 grcov . --binary-path ./target/debug/ -s . -t html -o target/coverage/html
-firefox target/coverage/html/index.html &
+
