c67b40cade045870b7b03d655f70e7da0ca87cf2
Ben Alexander <benalexander71792@gmail.com>
2023-12-30 17:46:36 -0800

    Working on coverage

---
+++ rust_game/test-and-coverage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export CARGO_INCREMENTAL=0
+export RUSTFLAGS='-Cinstrument-coverage'
+export LLVM_PROFILE_FILE='target/coverage/cargo-test-%p-%m.profraw' 
+
+cargo clean
+cargo test
+grcov . --binary-path ./target/debug/ -s . -t html -o target/coverage/html
